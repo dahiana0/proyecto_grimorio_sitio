ea5243c9a4ab7bdd34bf69a568ebfc9e98e79932
sofireyes05 <amayareyessofia@gmail.com>
2026-04-30 11:29:01 -0500

    fin

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -7,23 +7,24 @@ export const StatusGame = () => {
     const [gemas, setGemas] = useState();
 
     return (
-    <>
+        <>
+            <div className='puntos'>
+                <button>Punto 1 </button>
+                <button>Punto 2 </button>
+                <button>Punto 3 </button>
+            </div>
+            <div className='vidas'>
+                <button>vidas 1 </button>
+                <button>vidas 2 </button>
+                <button>vidas 3 </button>
+            </div>
             <div className='gemas '>
                 <button> Gemas 1</button>
                 <button> Gemas 2</button>
                 <button> Gemas 3</button>
-                <div className='puntos'>
-                    <button>Punto 1 </button>
-                    <button>Punto 2 </button>
-                    <button>Punto 3 </button>
-                </div>
-                <div className='vidas'>
-                    <button>vidas 1 </button>
-                    <button>vidas 2 </button>
-                    <button>vidas 3 </button>
-                </div>
-            </>
-            )
-    
+            </div>
+        </>
+    )
+
 }
 
