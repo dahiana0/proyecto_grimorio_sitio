878349110c77d501a8d26f6363ee6beda9ac1bb6
Karol Dahiana <usugakarondahiana@gmail.com>
2026-04-30 10:55:58 -0500

    hola

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-//yamile 😒
+//yamile 😒👌
 export const StatusGame = () => {
   const [puntos, setPuntos] = useState();
   const [vidas, setVidas] = useState();
