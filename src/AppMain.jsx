41c40315d835fd0b2c9824ea4c25369f7cb4a54b
sofireyes05 <amayareyessofia@gmail.com>
2026-05-04 10:33:34 -0500

    appmain

---
+++ src/AppMain.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AppMain = () => {
+  return (
+    <div>AppMain</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import {App} from './App.jsx'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
+import { AppMain } from './AppMain.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App />  */}
+    <AppMain/>
   </StrictMode>,
 )
