083a408a155a50296580f8fd9cd014de946739fa
Yamile0712 <yamileariasperez07@gmail.com>
2026-05-04 10:42:49 -0500

    hello

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { StatusGame } from './Components/StatusGame'
 
 export const AppMain = () => {
   return (
+    <>
+
     <div>AppMain</div>
+    <StatusGame/>
+    
+    </>
+   
   )
 }
