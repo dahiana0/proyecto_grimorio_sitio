b335a4b7bcf97ee40a8d94844edbf20f055a7d60
Yamile0712 <yamileariasperez07@gmail.com>
2026-05-06 11:05:05 -0500

    yamile

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StatusGame } from './Components/StatusGame'
+import { YamileRMpage } from './Pages/YamileRMpage'
 
 export const AppMain = () => {
   return (
@@ -7,6 +8,7 @@ export const AppMain = () => {
 
     <div>AppMain</div>
     <StatusGame/>
+    <YamileRMpage/>
     
     </>
    
