750e139691be350dcbaae5e271775410cec0d885
sofireyes05 <amayareyessofia@gmail.com>
2026-05-06 11:03:54 -0500

    statusgame-sofia

--- src/Pages/SofiaRMpage.jsx
+++ src/Pages/SofiaRMpage.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { StatusGame } from '../Components/Statusgame'
 
 export const SofiaRMpage = () => {
+
+    const [characters, Setcharacters] = useState ([])
+
+    const getCharacters = async() => {
+
+        const res = await fetch( "https://rickandmortyapi.com/api/character")
+        const data = res.json()
+
+        console.log(data)
+
+    }
+
+    useEffect(() => {
+      getCharacters()
+
+    }, [third])
+     
+
   return (
-    <div>SofiaRMpage</div>
+    <>
+     <StatusGame/>
+     <SofiaRMpage/>
+    </>
   )
 }
