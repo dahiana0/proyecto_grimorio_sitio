320bb9d352d5f36ba165ea418fa58bbed8634f17
Karol Dahiana <usugakarondahiana@gmail.com>
2026-05-06 11:32:15 -0500

    map karol

--- src/Pages/KarolRMP.jsx
+++ src/Pages/KarolRMP.jsx
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from 'react'
 
 export function KarolRMP() {
 
- const [character,setCharacter] = useState([])
- const getCharacters = async () => {
- const res = await fetch ("https://rickandmortyapi.com/api/character")
- const data = await res.json()
- console.log(data)
- }
+  const [character, setCharacter] = useState([])
+  const getCharacters = async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await res.json()
+    setCharacter(data.results)
+    console.log(data)
+  }
 
- useEffect(() => {
-  getCharacters()
- }, [])
- 
+  useEffect(() => {
+    getCharacters()
+  }, [])
+
 
   return (
+    <>
+      <h1> Personajes De Rick And Morty</h1>
+      <ul>
+        {character.map((char, index) => (
+          <li key={index}>
+            <p> {char.name}</p>
+          </li>
+        ))}
+      </ul>
+      )
+    </>
 
-    <div>KarolRMP </div>
   )
 }
