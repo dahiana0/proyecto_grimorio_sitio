7dd6fa7e8909635a75d352096d14537719990399
sofireyes05 <amayareyessofia@gmail.com>
2026-05-06 11:38:25 -0500

    chart sofia

--- src/Pages/SofiaRMpage.jsx
+++ src/Pages/SofiaRMpage.jsx
@@ -3,24 +3,35 @@ import { useEffect } from 'react'
 
 export const SofiaRMpage = () => {
 
-    const [characters, Setcharacters] = useState ([])
+    const [characters, Setcharacters] = useState([])
 
-    const getCharacters = async() => {
+    const getCharacters = async () => {
 
-        const res = await fetch( "https://rickandmortyapi.com/api/character")
+        const res = await fetch("https://rickandmortyapi.com/api/character")
         const data = await res.json()
-
+        Setcharacters(data.results)
         console.log(data)
 
     }
 
     useEffect(() => {
-      getCharacters
+        getCharacters
 
     }, [])
-     
 
-  return (
-    <div>SofiaRMpage</div>
-  )
+
+    return (
+        <>
+            <h1> Personajes de Rick and Morty </h1>
+            <ul>
+                {characters.map((char, index) => (
+
+                    <li key={index}>
+                        <p>{char.name}</p>
+                    </li>
+                ))}
+            </ul>
+
+        </>
+    )
 }
