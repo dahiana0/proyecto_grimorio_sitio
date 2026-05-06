e7ce0717396243078898fcb202e575f154976a16
Karol Dahiana <usugakarondahiana@gmail.com>
2026-05-06 11:24:13 -0500

    await karol

--- src/Pages/KarolRMP.jsx
+++ src/Pages/KarolRMP.jsx
@@ -5,7 +5,7 @@ export function KarolRMP() {
  const [character,setCharacter] = useState([])
  const getCharacters = async () => {
  const res = await fetch ("https://rickandmortyapi.com/api/character")
- const data = res.json()
+ const data = await res.json()
  console.log(data)
  }
 
