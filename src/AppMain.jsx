3283d5e1f70e0dbe37c7d9c11484c28fc70c3ec2
sofireyes05 <amayareyessofia@gmail.com>
2026-05-06 11:12:54 -0500

    pagesofia

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StatusGame } from './Components/StatusGame'
 import { YamileRMpage } from './Pages/YamileRMpage'
+import { SofiaRMpage } from './Pages/SofiaRMpage'
 
 export const AppMain = () => {
   return (
