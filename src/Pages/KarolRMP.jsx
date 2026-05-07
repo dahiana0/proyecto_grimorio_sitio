85ffef6bb2f9f85461cc5bb4c7bb9c44150f8702
Karol Dahiana <usugakarondahiana@gmail.com>
2026-05-07 11:17:40 -0500

    pokemons

--- src/Pages/KarolRMP.jsx
+++ src/Pages/KarolRMP.jsx
@@ -10,8 +10,17 @@ export function KarolRMP() {
     console.log(data)
   }
 
+  const [pokemons, setPokemons] = useState([])
+  const getPokemons = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
+    const data = await res.json()
+    setPokemons(data.results)
+    console.log(data)
+  }
+
   useEffect(() => {
     getCharacters()
+    getPokemons()
   }, [])
 
 
@@ -20,7 +29,7 @@ export function KarolRMP() {
       <h1> Personajes De Rick And Morty 😶‍🌫️ </h1>
 
       {character.map((char, index) => (
-        <div className="card" style={{ width: "18rem" }}>
+        <div key={index} className="card" style={{ width: "18rem" }}>
           <img src={char.image} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{char.name}</h5>
