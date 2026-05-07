7f60f04e93d4e953fa35f1b74457f1d83ea39d3d
sofireyes05 <amayareyessofia@gmail.com>
2026-05-07 10:31:08 -0500

    sofiapull

--- src/Pages/SofiaRMpage.jsx
+++ src/Pages/SofiaRMpage.jsx
@@ -17,6 +17,14 @@ export function SofiaRMpage() {
 
     return (
         <>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
             <h1> Personajes De Rick And Morty👌</h1>
             <ul>
                 {character.map((char, index) => (
