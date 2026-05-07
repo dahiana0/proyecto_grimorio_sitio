d62ceada481fb3aa711c5e55c6d063b809700517
Karol Dahiana <usugakarondahiana@gmail.com>
2026-05-07 10:53:46 -0500

    card

--- src/Pages/KarolRMP.jsx
+++ src/Pages/KarolRMP.jsx
@@ -20,7 +20,7 @@ export function KarolRMP() {
       <h1> Personajes De Rick And Morty</h1>
 
       {character.map((char, index) => (
-        <div className="card" style={{ width: "18rem;" }}>
+        <div className="card" style={{ width: "18rem" }}>
           <img src={char.image} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{char.name}</h5>
