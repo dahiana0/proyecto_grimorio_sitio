f2cd532d1e4804d57afcd3431df09a1a143a987b
sofireyes05 <amayareyessofia@gmail.com>
2026-05-07 10:23:04 -0500

    ultimosofia

--- src/Pages/SofiaRMpage.jsx
+++ src/Pages/SofiaRMpage.jsx
@@ -2,31 +2,31 @@ import React, { useEffect, useState } from 'react'
 
 export function SofiaRMpage() {
 
-  const [character, setCharacter] = useState([])
-  const getCharacters = async () => {
-    const res = await fetch("https://rickandmortyapi.com/api/character")
-    const data = await res.json()
-    setCharacter(data.results)
-    console.log(data)
-  }
+    const [character, setCharacter] = useState([])
+    const getCharacters = async () => {
+        const res = await fetch("https://rickandmortyapi.com/api/character")
+        const data = await res.json()
+        setCharacter(data.results)
+        console.log(data)
+    }
 
-  useEffect(() => {
-    getCharacters()
-  }, [])
+    useEffect(() => {
+        getCharacters()
+    }, [])
 
 
-  return (
-    <>
-      <h1> Personajes De Rick And Morty</h1>
-      <ul>
-        {character.map((char, index) => (
-          <li key={index}>
-            <p> {char.name}</p>
-          </li>
-        ))}
-      </ul>
-      
-    </>
+    return (
+        <>
+            <h1> Personajes De Rick And Morty👌</h1>
+            <ul>
+                {character.map((char, index) => (
+                    <li key={index}>
+                        <p> {char.name}</p>
+                    </li>
+                ))}
+            </ul>
 
-  )
+        </>
+
+    )
 }
