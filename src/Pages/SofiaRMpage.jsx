f49168cbad45ce77c1b458d0e4bd186e56212a4d
sofireyes05 <amayareyessofia@gmail.com>
2026-05-07 11:24:47 -0500

    species

--- src/Pages/SofiaRMpage.jsx
+++ src/Pages/SofiaRMpage.jsx
@@ -3,15 +3,15 @@ import React, { useEffect, useState } from 'react'
 export function SofiaRMpage() {
 
     const [character, setCharacter] = useState([])
-    const [pokemons, setPokemons] = useState([])
-
+    
     const getCharacters = async () => {
         const res = await fetch("https://rickandmortyapi.com/api/character")
         const data = await res.json()
         setCharacter(data.results)
         console.log(data)
     }
- 
+    
+    const [pokemons, setPokemons] = useState([])
     const getPokemons= async () => {
         const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
         const data = await res.json()
@@ -36,7 +36,7 @@ export function SofiaRMpage() {
                     <div className="card-body">
                         <h5 className="card-title">{char.name}</h5>
                         <p className="card-text">Status: {char.status}</p>
-                        <p className="card-text">Especie: {char}</p>
+                        <p className="card-text">Especie: {char.species}</p>
                     </div>
                 </div>
             ))}
