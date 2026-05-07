978df7ec9539e8bc6281512a1f06aa36e2292931
Karol Dahiana <usugakarondahiana@gmail.com>
2026-05-07 10:29:49 -0500

    card karol

--- src/Pages/KarolRMP.jsx
+++ src/Pages/KarolRMP.jsx
@@ -17,6 +17,15 @@ export function KarolRMP() {
 
   return (
     <>
+
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
       <h1> Personajes De Rick And Morty</h1>
       <ul>
         {character.map((char, index) => (
@@ -25,7 +34,7 @@ export function KarolRMP() {
           </li>
         ))}
       </ul>
-      
+
     </>
 
   )
