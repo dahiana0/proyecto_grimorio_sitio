fd3414a30913bbc1742fbc1b8e4c2433bf34e3c3
Karol Dahiana <usugakarondahiana@gmail.com>
2026-05-08 11:28:41 -0500

    karol cambios 2

--- src/Pages/KarolRMP.jsx
+++ src/Pages/KarolRMP.jsx
@@ -10,28 +10,30 @@ export function KarolRMP() {
     console.log(data)
   }
 
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]);
+
+
   const getPokemons = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/limit=12")
-    const data = await res.json()
-    
-    const detallePokemon = await Promise.all(
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+    const data = await res.json();
+
+    const detallesPokemon = await Promise.all(
       data.results.map(async (pokemon) => {
-        const res = await fetch(pokemon.url)
-        const dettales = await res.json()
+        const res = await fetch(pokemon.url);
+        const detalles = await res.json();
 
         return {
           id: detalles.id,
           name: detalles.name,
-          image: detalles.sprites.other["official-artwork"].front_defaul,
-          type: detalles.types[0].type.name
-        }
-
+          image: detalles.sprites.other["official-artwork"].front_default,
+          type: detalles.types[0].type.name,
+        };
       })
-    )
-    setPokemons(detallespokemon)
-    console.log(data)
-  }
+    );
+
+    setPokemons(detallesPokemon);
+    console.log(detallesPokemon);
+  };
 
   useEffect(() => {
     getCharacters()
@@ -53,29 +55,32 @@ export function KarolRMP() {
           </div>
         </div>
       ))}
+     gi
+     <h1> Tarjetas Pokemon 🐹</h1>
 
+      {pokemons.map((poke, index) => (
+        <div key={index} className="card" style={{ width: "18rem" }}>
+          <img src={poke.image} className="card-img-top" alt="..." />
+
+          <div className="card-body">
+            <h5 className="card-title">{poke.name}</h5>
+            <p className="card-text">Status: {poke.type}</p>
+            <p className="card-text">Especie: {poke.id}</p>
+          </div>
+        </div>
+      ))}
 
     </>
 
   )
+
+  
+    
+      
+
+
+    
+
+    
 }
 
-return (
-  <>
-    <h1> Tarjetas Pokemon 🐹</h1>
-
-    {pokemons.map((poke, index) => (
-      <div key={index} className="card" style={{ width: "18rem" }}>
-        <img src={poke.image} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{poke.name}</h5>
-          <p className="card-text">Status: {poke.type}</p>
-          <p className="card-text">Especie: {poke.id}</p>
-        </div>
-      </div>
-    ))}
-
-
-  </>
-
-)
