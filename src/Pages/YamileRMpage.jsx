8e5df87d54da0fd8bb34fa92416d7ce2650452fd
Yamile0712 <yamileariasperez07@gmail.com>
2026-05-08 11:29:31 -0500

    yamile ca,bio 3

--- src/Pages/YamileRMpage.jsx
+++ src/Pages/YamileRMpage.jsx
@@ -13,7 +13,6 @@ export const YamileRMpage = () => {
 
   const [pokemons, setPokemons] = useState([]);
 
-
   const getPokemons = async () => {
     const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
     const data = await res.json();
@@ -29,7 +28,7 @@ export const YamileRMpage = () => {
           image: detalles.sprites.other["official-artwork"].front_default,
           type: detalles.types[0].type.name,
         };
-      })
+      }),
     );
 
     setPokemons(detallesPokemon);
@@ -49,7 +48,6 @@ export const YamileRMpage = () => {
         <div key={index} className="card" style={{ width: "18rem" }}>
           <img src={char.image} className="card-img-top" alt="..." />
           <div className="card-body">
-
             <h5 className="card-title">{char.name}</h5>
             <p className="card-text">Status: {char.status}</p>
             <p className="card-text">Especie: {char.specie}</p>
@@ -58,18 +56,21 @@ export const YamileRMpage = () => {
       ))}
 
       <h1>Card De Pokemons Yamile </h1>
-      {pokemons.map((poke, index) => (
-        <div key={index} className="card" style={{ width: "18rem" }}>
-          <img src={poke.image} className="card-img-top" alt="..." />
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {pokemons.map((poke, index) => (
+            <div key={index} className="card" style={{ width: "18rem" }}>
+              <img src={poke.image} className="card-img-top" alt="..." />
 
-          <div className="card-body">
-            <h5 className="card-title">{poke.name}</h5>
-            <p className="card-text">Status: {poke.type}</p>
-            <p className="card-text">Especie: {poke.id}</p>
-          </div>
+              <div className="card-body">
+                <h5 className="card-title">{poke.name}</h5>
+                <p className="card-text">Status: {poke.type}</p>
+                <p className="card-text">Especie: {poke.id}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
-
