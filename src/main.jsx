3186d49d2f7a26c65bfca286148600b15d87b573
Yamile0712 <yamileariasperez07@gmail.com>
2026-05-15 10:21:43 -0500

    se creo el juego yamile

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import {App} from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import { AppMain } from './AppMain.jsx'
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App />  */}
-    <AppMain/>
+    <App />
+    {/* <AppMain/>*/}
   </StrictMode>,
-)
+);
