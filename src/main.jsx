b18bfca715228c0d830936f08487504b8cff5e55
Yamile0712 <yamileariasperez07@gmail.com>
2026-05-15 10:39:45 -0500

    se agrego correciones de equipo - yamile

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,9 @@ import {App} from './App.jsx'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
-import { AppMain } from './AppMain.jsx'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< HEAD
-    {/* <App />  */}
-    <AppMain/>
-  </StrictMode>,z
-)
-=======
     <App />
-    {/* <AppMain/>*/}
-  </StrictMode>,
+  </StrictMode>
 );
->>>>>>> 8b22931c96020dc3419e36c565967fbdc7b7e80d
+
