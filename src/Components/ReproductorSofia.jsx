995dad6f69c2f436adbb2b0de60f1feb5c52c86d
sofireyes05 <amayareyessofia@gmail.com>
2026-05-27 10:21:03 -0500

    reproductor

---
+++ src/Components/ReproductorSofia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const ReproductorSofia = () => {
+  return (
+    <>
+    <h1> Reproductor interactivo</h1>
+    <video width={700}>
+        <source src="" />
+    </video>
+
+    <div>
+        <button>Play</button>
+        <button>Pause</button>
+    </div>
+    </>
+  )
+}
