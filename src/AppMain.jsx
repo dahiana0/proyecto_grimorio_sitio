cf43da97b697929a71574d5a7cbcb8050905dc6e
sofireyes05 <amayareyessofia@gmail.com>
2026-05-27 10:37:17 -0500

    reproductor sofia agregado

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -3,15 +3,17 @@ import { StatusGame } from "./Components/StatusGame";
 import { YamileRMpage } from "./Pages/YamileRMpage";
 import { SofiaRMpage } from "./Pages/SofiaRMpage";
 import { KarolRMP } from "./Pages/KarolRMP";
+import { ReproductorSofia } from "./Components/ReproductorSofia";
 
 export const AppMain = () => {
   return (
     <>
-      <div>AppMain</div>
+     {/*  <div>AppMain</div>
       <KarolRMP />
       <h1>ddgfr</h1>
       <YamileRMpage />
-      <SofiaRMpage />
+      <SofiaRMpage /> */}
+      <ReproductorSofia/>
     </>
   );
 };
