d0ff63739ec736d0c340ceb522317ac4cce74230
Karol Dahiana <usugakarondahiana@gmail.com>
2026-06-03 10:57:08 -0500

    karol

--- src/Components/LootieKarol.jsx
+++ src/Components/LootieKarol.jsx
@@ -1,13 +1,24 @@
-import Lottie from "lottie-react";
+import LottieMoudule from "lottie-react";
 import peloticaKarol from "../assets/data-Karol.json"
+import { useRef } from "react";
+
+const Lottie = LottieMoudule.default;
 
 
 export const LootieKarol = () => {
+  const lottieRef = useRef()
+
+  const reproducir =() =>{
+    lottieRef.current.stop()
+    lottieRef.current.play()
+  }
   return (
-    <div>
+  
+    <div onClick={reproducir}>
         <Lottie
+        lottieRef={lottieRef}
         animationData={peloticaKarol}
-        loop={true}
+        loop={false}
         style={{width:500, height: 500}}
         />
     </div>
