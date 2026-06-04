eaff849829cfae01f4336c4196e6764e74da7d55
Karol Dahiana <usugakarondahiana@gmail.com>
2026-06-04 10:37:29 -0500

    karol

--- src/Components/VideoIntroductorio.jsx
+++ src/Components/VideoIntroductorio.jsx
@@ -1,45 +1,74 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "../Styles/css.css";
 
 export const VideoIntroductorio = () => {
+  const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
 
-  const videoURL = "https://www.youtube.com/embed/Jek5K3xqEFE";
+  const handlePlayPause = () => {
+    if (play) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setPlay(!play);
+  };
+
+  const handleForward = () => {
+    videoRef.current.currentTime += 10;
+  };
+
+  const handleBackward = () => {
+    videoRef.current.currentTime -= 10;
+  };
 
   return (
-    <div className="video-container d-flex flex-column align-items-center">
+    <div className="video-container">
 
       <h2 className="video-title">
         VIDEO INTRODUCTORIO
       </h2>
 
-      <div
-        className="video-box"
-        onClick={() => setPlay(true)}
-      >
-        {!play ? (
-          <>
-            <img
-              src="./Fondoinicio.png"
-              alt="Video"
-              className="video-img"
-            />
-
-            <div className="video-play">
-              <img src="./player-play.svg" className="video-icon" />
-            </div>
-          </>
-        ) : (
-          <iframe
-            width="100%"
-            height="100%"
-            src={videoURL + "?autoplay=1"}
-            title="Video Intro"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
+      <div className="grimorio-portal">
+        <video
+          ref={videoRef}
+          className="video-player"
+        >
+          <source
+            src="/VideoIntroductorio.mp4"
+            type="video/mp4"
           />
-        )}
+        </video>
       </div>
+
+      <div className="video-controls">
+
+        <button onClick={handleBackward}>
+          <img
+            src="/retroceder.png"
+            alt="Retroceder"
+          />
+        </button>
+
+        <button
+          className="play-button"
+          onClick={handlePlayPause}
+        >
+          <img
+            src={play ? "/pause.png" : "/play.png"}
+            alt="Play Pause"
+          />
+        </button>
+
+        <button onClick={handleForward}>
+          <img
+            src="/adelantar.png"
+            alt="Adelantar"
+          />
+        </button>
+
+      </div>
+
     </div>
   );
 };
