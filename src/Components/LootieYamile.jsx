d12fd46a31644e7c1b955c4a21c88c5a94220a99
Yamile0712 <yamileariasperez07@gmail.com>
2026-06-05 11:10:18 -0500

    yamile pelota

--- src/Components/LootieYamile.jsx
+++ src/Components/LootieYamile.jsx
@@ -1,14 +1,32 @@
-import Lottie from "lottie-react";
+import LottieModule  from "lottie-react";
 import peloticaYamile from "../assets/data-yamile.json";
+import { useRef } from "react";
 
+const Lottie = LottieModule.default;
 export const LootieYamile = () => {
+
+  const lottieRef = useRef()
+
+  const reproducir=()=>{
+    
+    lottieRef.current.stop()
+    lottieRef.current.play()
+  }
+
   return (
-    <div>
-      <Lottie
-        animationData={peloticaYamile}
-        loop={true}
-        style={{ width: 500, height: 500 }}
-      />
-    </div>
+    <>
+      <div onClick={reproducir}>
+        <Lottie
+          lottieRef={lottieRef}
+          animationData={peloticaYamile}
+          loop={false}
+          autoplay={false}
+          style={{
+            width: 500,
+            height: 500,
+          }}
+        />
+      </div>
+    </>
   );
 };
